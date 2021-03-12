904dfb10aaa1f7d90f9deafd80b8a7ae0b5babfa
MarcMerino <marcmerino24@gmail.com>
2021-03-12 17:48:29 +0100

    Fixed login CSS and implemented login with api authentication

--- www/js/login.js
+++ www/js/login.js
@@ -1,43 +1,35 @@
 document.addEventListener('deviceready', onDeviceReady, false);
+var btnLogin = document.getElementById("loginButton"); btnLogin.onclick = login;
+var email = document.getElementById("email");
+var password = document.getElementById("password");
 
 function login() {
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-
-    validateLogin(email.value, CryptoJS.SHA256(password.value).toString());
-}
-
-function validateUser(email, password) {
-    let query = "users";
-    $.ajax({
-        method: "GET",
-        url: "API" + query,
-        dataType: "json", // necessitem això pq ens retorni un objecte JSON
-    }).done(function(user) {
-        for (let i = 0; i < user.length; i++) {
-            if (user[i].email == email && user[i].password == password) {
-                console.log("email: " + user[i].email + " -- password: " + user[i].password);
-                return true;
-            }
-        }
-
-        return false;
-    }).fail(function() {
-        alert("ERROR");
-    });
+    validateLogin(email.value, CryptoJS.SHA256(password.value).toString());    
 }
 
 function validateLogin(email, password) {
-    let validUser = validateUser(email, password);
+    let query = "/users?email="+email;
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:3000" + query,
+        dataType: "json",
+    }).done(function(user) {
+        if (user.length > 0) {
+            for (let i = 0; i < user.length; i++) {
+                if (user[i].email == email && user[i].password == password) {
+                    alert("Login correct");
+                }
+            }
+        } else {
+            alert("Login incorrect");
 
-    if (validUser == true) {
-        alert("Login Correct");
-    } else {
-        alert("Login Incorrect")
-    }
+        }
+        
+    }).fail(function() {
+        alert("No se ha entrado ningun usuario con ese email.");
+    });
 }
 
-
 function onDeviceReady() {
-    var btnLogin = document.getElementById("loginButton").onclick = login;
+    // Pass
 }
